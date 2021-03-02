9d6d1ca302998c9db704dc842d78351d62881b07
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2021-03-02 15:39:23 +0300

    реализовал панель фильтрации

--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -5,14 +5,22 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 import './Switcher.css'
 
-export const Switcher = () => {
+export const Switcher = ({onFilterTodo}) => {
+
+  const statuses = [
+    {name: 'All', status: 'all'},
+    {name: 'Done', status: 'done'},
+    {name: 'In process', status: 'process'},
+  ]
+
+  const btns = statuses.map( (item, index) =>  <Button key={index} onClick={ () => onFilterTodo(item.status) }>{item.name}</Button>)
+
   return (
     <div className='btn-switcher'>
       <ButtonGroup size="sm">
-        <Button>All</Button>
-        <Button>Done</Button>
-        <Button>In process</Button>
+        {btns}
       </ButtonGroup>
+
     </div>
   )
 }
